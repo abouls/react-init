ed06f3af78a874e200d47157bee988f14caf634c
雾塔 <wutayunfan@gmail.com>
2016-09-02 10:58:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // This is the entry file
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      {/* Replace the div markup with your own components of react */}
+      <div>hey, ho, let\'s go!!!</div>  
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
